3eb7f176a4cc533f927bda4f9d6d75aa3db236f4
ReeceWoodard <rwoodar5@students.kennesaw.edu>
2026-06-19 19:07:00 -0400

    Update Assignment2.py

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,3 +1,9 @@
+# Program Name: Assignment2.py
+# Course: IT3883/Section W01
+# Student Name: Reece Woodard
+# Assignment Number: Lab2
+# Due Date: 6/19/2026
+# Purpose: Calculates the averages for a list of students grades and prints the results in descending order.
 
 file_name = "input.txt"
 
