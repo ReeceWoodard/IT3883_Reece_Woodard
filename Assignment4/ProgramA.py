3da9a9160f72cf9aebae3edc2b1bbbcc57310a2e
ReeceWoodard <rwoodar5@students.kennesaw.edu>
2026-07-10 19:19:54 -0400

    Update ProgramA.py

--- Assignment4/ProgramA.py
+++ Assignment4/ProgramA.py
@@ -1,1 +1,18 @@
-x
+import socket
+
+HOST = "127.0.0.1"
+PORT = 45000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+message = input("Enter a string: ")
+
+client.send(message.encode())
+
+response = client.recv(1024).decode()
+
+print("Response from Program B:")
+print(response)
+
+client.close()
